19cc78060124ae778234a8e4dad85dd6402eb022
janetchang <janetlaichang@gmail.com>
2013-04-08 12:20:07 -0700

    DRY up Person spec

---
+++ spec/person_spec.rb
@@ -0,0 +1,51 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../person'
+
+describe "Person" do
+  before do
+    @person                        = Person.new({ :last_name => "Chang", :first_name => "Bobert", :gender => "Male", :birthdate => "11/26/1990", :fave_color => "blue", :middle_initial => "A" })
+    @person_no_middle_initial      = Person.new({ :last_name => "Cyrus", :first_name => "Miley", :gender => "Female", :birthdate => "1/1/1995", :fave_color => "pink" })
+    @person_non_standard_gender    = Person.new({ :last_name => "Bonk", :first_name => "Radek", :gender => "M", :birthdate => "6/3/1975", :fave_color => "green", :middle_initial => "S" })
+    @person_non_standard_birthdate = Person.new({ :birthdate => "3-3-1985", :gender => "M" })
+    @person_single_digit_birthdate = Person.new({ :birthdate => "1/4/1954", :gender => "M" })
+  end
+
+  it "stores information about the person" do
+    @person.last_name.must_equal "Chang"
+    @person.first_name.must_equal "Bobert"
+    @person.gender.must_equal "Male"
+    @person.birthdate.must_equal "11/26/1990"
+    @person.fave_color.must_equal "blue"
+    @person.middle_initial.must_equal "A"
+  end
+
+  it "retains information securely" do
+    ATTRIBUTES = ["last_name", "first_name", "gender", "birthdate", "fave_color"]
+    ATTRIBUTES.each do |a|
+      proc { @person.a = "Hacker attempts to change attribute" }.must_raise(NoMethodError)
+    end
+  end
+
+  it "stores information without requiring middle initial" do
+    @person_no_middle_initial.last_name.must_equal "Cyrus"
+    @person_no_middle_initial.first_name.must_equal "Miley"
+    @person_no_middle_initial.gender.must_equal "Female"
+    @person_no_middle_initial.birthdate.must_equal "1/1/1995"
+    @person_no_middle_initial.fave_color.must_equal "pink"
+    @person_no_middle_initial.middle_initial.must_equal nil
+  end
+
+  it "correctly formats genders" do
+    @person_non_standard_gender.gender.must_equal "Male"
+  end
+  
+  it "correctly formats birthdates" do
+    @person_non_standard_birthdate.birthdate.must_equal "3/3/1985"
+  end
+
+  it "provides sortable version of birthdate" do
+    @person.sortable_birthdate.must_equal("1990/11/26")
+    @person_single_digit_birthdate.sortable_birthdate.must_equal("1954/01/04")
+  end
+end
---
+++ spec/personcontroller_spec.rb
@@ -0,0 +1,168 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../personcontroller'
+require_relative '../person'
+
+describe "PersonController" do
+  before do
+    @birthdate_sorted_data = File.open("output_birthdate_ascending.txt").read
+    @gender_sorted_data = File.open("output_gender_f_m.txt").read
+    @lastname_sorted_data = File.open("output_lastname_descending.txt").read
+
+    @orig_pipe_person1 = "Smith | Steve | D | M | Red | 3-3-1985\n"
+    @orig_pipe_person2 = "Bonk | Radek | S | M | Green | 6-3-1975\n"
+    @orig_pipe_person3 = "Bouillon | Francis | G | M | Blue | 6-3-1975"
+    
+    @orig_comma_person1 = "Abercrombie, Neil, Male, Tan, 2/13/1943\n"
+    @orig_comma_person2 = "Bishop, Timothy, Male, Yellow, 4/23/1967\n"
+    @orig_comma_person3 = "Kelly, Sue, Female, Pink, 7/12/1959"
+
+    @orig_space_person1 = "Kournikova Anna F F 6-3-1975 Red\n"
+    @orig_space_person2 = "Hingis Martina M F 4-2-1979 Green\n"
+    @orig_space_person3 = "Seles Monica H F 12-2-1973 Black"
+
+    @prepped_pipe_attributes1 = { :last_name => "Smith", :first_name => "Steve", :gender => "M", :birthdate => "3-3-1985", :fave_color => "Red", :middle_initial => "D" }
+    @prepped_pipe_attributes2 = { :last_name => "Bonk", :first_name => "Radek", :gender => "M", :birthdate => "6-3-1975", :fave_color => "Green", :middle_initial => "S" }
+    @prepped_pipe_attributes3 = { :last_name => "Bouillon", :first_name => "Francis", :gender => "M", :birthdate => "6-3-1975", :fave_color => "Blue", :middle_initial => "G" }
+  
+    @prepped_comma_attributes1 = { :last_name => "Abercrombie", :first_name => "Neil", :gender => "Male", :birthdate => "2/13/1943", :fave_color => "Tan" }
+    @prepped_comma_attributes2 = { :last_name => "Bishop", :first_name => "Timothy", :gender => "Male", :birthdate => "4/23/1967", :fave_color => "Yellow" }
+    @prepped_comma_attributes3 = { :last_name => "Kelly", :first_name => "Sue", :gender => "Female", :birthdate => "7/12/1959", :fave_color => "Pink" }
+
+    @prepped_space_attributes1 = { :last_name => "Kournikova", :first_name => "Anna", :middle_initial => "F", :gender => "F", :birthdate => "6-3-1975", :fave_color => "Red"}
+    @prepped_space_attributes2 = { :last_name => "Hingis", :first_name => "Martina", :middle_initial => "M", :gender => "F", :birthdate => "4-2-1979", :fave_color => "Green"}
+    @prepped_space_attributes3 = { :last_name => "Seles", :first_name => "Monica", :middle_initial => "H", :gender => "F", :birthdate => "12-2-1973", :fave_color => "Black"}
+
+    @person1 = Person.new(@prepped_space_attributes2) #hingis martina
+    @person2 = Person.new(@prepped_comma_attributes3) #kelly sue
+    @person3 = Person.new(@prepped_space_attributes1) #kournikova anna
+    @person4 = Person.new(@prepped_space_attributes3) #seles monica
+    @person5 = Person.new(@prepped_comma_attributes1) #abercrombie, neil
+    @person6 = Person.new(@prepped_comma_attributes2) #bishop timothy
+    @person7 = Person.new(@prepped_pipe_attributes2) #bonk redik
+    @person8 = Person.new(@prepped_pipe_attributes3) #bouillon francis
+    @person9 = Person.new(@prepped_pipe_attributes1) #smith steve
+
+    @pipe = PersonController.new("pipe.txt", "pipe")
+    @comma = PersonController.new("comma.txt", "comma")
+    @space = PersonController.new("space.txt", "space")
+
+    def uploaded_and_parsed_people
+      @pipe.add_file("comma.txt", "comma")
+      @pipe.add_file("space.txt", "space")
+      @pipe.parsed
+      @pipe
+    end
+  end
+
+  describe "initialization" do
+    it "retains information about initial file" do
+      people = PersonController.new("pipe.txt", "pipe")
+      people.files.must_equal [{ :name => "pipe.txt", :type => "pipe" }]
+    end
+
+    # it "requires text file as input" do
+    #   expect{PersonController.new("blah.png", "pipe")}.to_raise WrongFileFormatError
+    # end
+
+    # it "requires valid file type as input" do
+    #   expect{PersonController.new("blah.png", "pipe")}.to_raise NoFileTypeError
+    # end
+  end
+
+  describe "adding new files" do
+    it "retains information about multiple files" do
+      people = PersonController.new("pipe.txt", "pipe")
+      people.add_file("comma.txt", "comma")
+      people.files.must_equal [{ :name => "pipe.txt", :type => "pipe" }, { :name => "comma.txt", :type => "comma" }]
+      people.add_file("space.txt", "space")
+      people.files.must_equal [{ :name => "pipe.txt", :type => "pipe" }, { :name => "comma.txt", :type => "comma" }, { :name => "space.txt", :type => "space" }]
+    end
+  end
+
+  describe "when parsing individual person data" do
+    it "handles pipe delimited files" do
+
+      attributes_to_hash(@orig_pipe_person1, "pipe").must_equal(@prepped_pipe_attributes1)
+      attributes_to_hash(@orig_pipe_person2, "pipe").must_equal(@prepped_pipe_attributes2)
+      attributes_to_hash(@orig_pipe_person3, "pipe").must_equal(@prepped_pipe_attributes3)
+    end
+
+    it "handles comma delimited files" do
+      attributes_to_hash(@orig_comma_person1, "comma").must_equal(@prepped_comma_attributes1)
+      attributes_to_hash(@orig_comma_person2, "comma").must_equal(@prepped_comma_attributes2)
+      attributes_to_hash(@orig_comma_person3, "comma").must_equal(@prepped_comma_attributes3)
+    end
+
+    it "handles space delimited files" do
+      attributes_to_hash(@orig_space_person1, "space").must_equal(@prepped_space_attributes1)
+      attributes_to_hash(@orig_space_person2, "space").must_equal(@prepped_space_attributes2)
+      attributes_to_hash(@orig_space_person3, "space").must_equal(@prepped_space_attributes3)
+    end
+  end
+
+  describe "when parsing and storing data" do
+    it "stores people information from pipe delimited files" do
+      @pipe.people.must_be_empty
+      @pipe.parsed
+      @pipe.people.wont_be_empty
+    end
+
+    it "stores people information from comma delimited files" do
+      @comma.people.must_be_empty
+      @comma.parsed
+      @comma.people.wont_be_empty
+    end
+
+    it "stores people information from space delimited files" do
+      @space.people.must_be_empty
+      @space.parsed
+      @space.people.wont_be_empty
+    end
+
+    it "remembers parsed files" do
+      uploaded_and_parsed_people.parsed_files.length.must_equal(3)
+    end
+  end
+
+  describe "making output files" do
+    it "shows by gender" do
+      uploaded_and_parsed_people.save_and_view_by("gender")
+      File.open("output_gender.txt").read.must_equal(@gender_sorted_data)
+    end
+    
+    it "shows by birthdate" do
+      uploaded_and_parsed_people.save_and_view_by("birthdate")
+      File.open("output_birthdate.txt").read.must_equal(@birthdate_sorted_data)
+    end
+
+    it "shows by last name" do
+      uploaded_and_parsed_people.save_and_view_by("last_name")
+      File.open("output_last_name.txt").read.must_equal(@lastname_sorted_data)
+    end
+  end
+
+  describe "sorting people" do
+    #NOTE: Tests in here should "fail" with "No visible difference" message. This is considered a passing test.
+    #Any other failures should be considered actual failure.
+
+    it "sorts by gender and fail this test with message: No visible difference." do
+      sorted_mock = [@person1, @person2, @person3, @person4, @person5, @person6, @person7, @person8, @person9]
+      uploaded_and_parsed_people.sorted_people("gender").must_equal(sorted_mock)
+    end
+
+    it "sorts by birthdate and fail this test with message: No visible difference." do
+      sorted_mock = [@person5, @person2, @person6, @person4, @person7, @person8, @person3, @person1, @person9]
+      uploaded_and_parsed_people.sorted_people("birthdate").must_equal(sorted_mock)
+    end
+
+    it "sorts by last name and fail this test with message: No visible difference." do
+      sorted_mock = [@person9, @person4, @person3, @person2, @person1, @person8, @person7, @person6, @person5]
+      uploaded_and_parsed_people.sorted_people("last_name").must_equal(sorted_mock)
+    end
+  end
+end
+
+
+
+
